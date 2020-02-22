1bc55d85c176857a32070431a093b9d9c0f6d36f
Sana Ebadi <sanaebadi97@gmail.com>
2020-02-22 10:06:07 +0330

    added data source bookmark interface and methods

---
+++ core/src/main/java/com/raywenderlich/android/majesticreader/data/BookmarkDataSource.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.android.majesticreader.data
+
+import com.raywenderlich.android.majesticreader.domain.Bookmark
+import com.raywenderlich.android.majesticreader.domain.Document
+
+interface BookmarkDataSource{
+
+    suspend fun add(document: Document, bookmark: Bookmark)
+    suspend fun read(document: Document) :List<Document>
+    suspend fun remove(document: Document)
+}
